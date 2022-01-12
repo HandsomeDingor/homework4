2e6dec9d64a919c465e8ba1c957823ef50349ef0
HandsomeDingor <jay807541931@gmail.com>
2022-01-12 01:59:05 -0500

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-const startButton = document.getElementById("start")
+const startButtonEl = document.getElementById("start-btn")
+const questionEl = document.getElementById("question");
+const startEl = document.getElementById("start")
+const questioncontainerEl = document.getElementById("question-container")
 
-startButton.addEventListener('click', startQuiz)
+
+startButtonEl.addEventListener('click', startQuiz)
 
 function startQuiz(){
-    startButton.style.display= "none";
+    startButtonEl.style.display= "none";
+    startEl.style.display="none";
+    questioncontainerEl.style.display="flex";
+
 }
+
